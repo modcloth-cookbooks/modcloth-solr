c6a79dc6c0589d3bd4486824f8e74aef08bf24de
Eric Saxby <sax@livinginthepast.org>
2013-09-19 19:36:28 -0700

    bump to 2.2.0

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "ops@modcloth.com"
 license          "Apache 2.0"
 description      "Installs/Configures solr"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "2.1.0"
+version          "2.2.0"
 
 depends "java"
 depends "smf", '>= 1.0.0'
